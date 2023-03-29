8e45fbe9cc29e5ea4b62178a34d3f12aab260682
vinayakgarole <vinayakgarole10@gamil.com>
2023-03-29 18:09:16 +0530

    Modified: Async nature of JS

--- AjaxServerCall/Timer.js
+++ AjaxServerCall/Timer.js
@@ -1,6 +1,6 @@
 function showTime() {
     const date = new Date();
-    return date.getHours() + " Hrs: " +date.getMinutes() + " Mins: " +date.getSeconds() + " Secs";
+    return date.getHours() + " Hrs: " +date.getMinutes() + " Mins: " + date.getSeconds() + " Secs";
 }
 
 function showSessionExpire() {
